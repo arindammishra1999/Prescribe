4b55fe8c6b5dee6c53df7e7d5e7d4699f2ea1be1
Saud Agha <saud.agha1@ucalgary.ca>
2024-03-30 10:50:28 -0600

    feat(frontend): desktop login page
    additional setup completed as well. added react-icons library for icons.
    
    Refs: PRES-6

---
+++ frontend/app/app/components/EmailInput.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function EmailInput() {
+  return (
+    <div className="mb-4">
+      <h6 className="ml-2">Email</h6>
+      <input
+        type="email"
+        placeholder="john@example.com"
+        className="w-full border border-gray-300 p-4 rounded-full"
+      />
+    </div>
+  );
+}
