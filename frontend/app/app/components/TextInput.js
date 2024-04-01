1c768f63a7e22e9fad4975e3bb17034edcc7ee17
Saud Agha <saud.agha1@ucalgary.ca>
2024-04-01 02:54:40 -0600

    PRES-8 feat(frontend): signup page
    Added signup pages for both mobile and desktop.

---
+++ frontend/app/app/components/TextInput.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const TextInput = ({ onInputChange, placeholder, fieldName }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    onInputChange(value);
+  };
+
+  return (
+    <div className="mb-4">
+      <h6 className="ml-2">{fieldName}</h6>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder={placeholder}
+        className="w-full bg-neutral-50 border border-white-300 p-4 rounded-full focus:border-teal-900 focus:outline-none"
+      />
+    </div>
+  );
+};
+
+export default TextInput;
