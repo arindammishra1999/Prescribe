838a22f94c2312d65416eb731baf86bdcbee372f
arindammishra1999 <arimishra1999@gmail.com>
2024-04-03 20:03:17 -0600

    PRES 14: fix prescription styling

--- frontend/app/app/doctorPage/prescriptionHistory/page.js
+++ frontend/app/app/doctorPage/prescriptionHistory/page.js
@@ -9,13 +9,14 @@ const Page = () => {
             <FiArrowLeft className="text-4xl ml-5 cursor-pointer absolute top-0 left-0 mt-4 ml-4" />
             <h1 className="text-3xl font-bold mb-4">Prescription History</h1>
 
-            <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-700 text-white w-96 h-60">
-                <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-700 text-white w-96 h-28 border-b-2 border-white flex items-center justify-between mb-5">
+            <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-900 text-white w-96 h-60">
+                <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-900 text-white w-96 h-28 border-b-2 border-white flex items-center justify-between mb-5">
                     <div className="block rounded-lg flex flex-col rounded-xl">
-                        <div className="border-2 border-teal-700 rounded-xl p-4 flex flex-row justify-evenly items-center text-white">
+                        <div className="border-2 border-teal-900 rounded-xl p-4 flex flex-row justify-evenly items-center text-white">
                             <FontAwesomeIcon
                                 icon={faPills}
-                                style={{ width: "5rem", marginRight: "3rem" }}
+                                size="5x"
+                                style={{ marginRight: "3rem" }}
                             />
                             <h1 className="text-xl font-bold text-white">
                                 Drug Z
@@ -48,13 +49,14 @@ const Page = () => {
                 </div>
             </div>
 
-            <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-700 text-white w-96 h-60">
-                <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-700 text-white w-96 h-28 border-b-2 border-white flex items-center justify-between mb-5">
+            <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-900 text-white w-96 h-60">
+                <div className="rounded-3xl overflow-hidden shadow-lg bg-teal-900 text-white w-96 h-28 border-b-2 border-white flex items-center justify-between mb-5">
                     <div className="block rounded-lg flex flex-col rounded-xl">
-                        <div className="border-2 border-teal-700 rounded-xl p-4 flex flex-row justify-evenly items-center text-white">
+                        <div className="border-2 border-teal-900 rounded-xl p-4 flex flex-row justify-evenly items-center text-white">
                             <FontAwesomeIcon
                                 icon={faPills}
-                                style={{ width: "5rem", marginRight: "3rem" }}
+                                size="5x"
+                                style={{ marginRight: "3rem" }}
                             />
                             <h1 className="text-xl font-bold text-white">
                                 Drug Z
