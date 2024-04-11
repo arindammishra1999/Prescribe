0269b43c05f6e159dca2f0769fabf5f38a214c74
Azlan Amjad <azlan.amjad1@ucalgary.ca>
2024-04-11 00:59:03 -0600

    PRES-20 Refactor error handling in loginController.js, loginService.js, and authorizeMiddleware.js

--- backend/services/user/app/controllers/loginController.js
+++ backend/services/user/app/controllers/loginController.js
@@ -6,13 +6,13 @@ const loginController = async (req, res) => {
         const {user, role} = req.body;
 
         if (!user.id || !user.password || !role) {
-            return res.status(400).send('Missing required fields');
+            return res.status(400).send({error: 'Missing required fields'});
         }
 
-        const authUser = await loginService(user.id, user.password, role);
+        const {user: authUser, error} = await loginService(user.id, user.password, role);
 
-        if (!authUser) {
-            return res.status(401).send('Unauthorized');
+        if (error) {
+            return res.status(401).send({error: error});
         }
 
         jwt.sign({id: user.id, role}, process.env.PRIVATE_KEY, {
@@ -23,14 +23,14 @@ const loginController = async (req, res) => {
         }, (error, token) => {
             if (error) {
                 console.error(error);
-                return res.status(500).send(error);
+                return res.status(500).send({error: error});
             }
             console.log(`Token: ${token} created for ${user.id}`);
             return res.status(200).send({token, user: authUser});
         });
     } catch (error) {
         console.error(error);
-        return res.status(500).send('Internal server error');
+        return res.status(500).send({error: 'Internal server error'});
     }
 }
 
