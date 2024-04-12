e2a59c1074f6051c9197e3b2e2e5e450c6d19908
deost <sajid.hafiz1@ucalgary.ca>
2024-04-12 15:58:55 -0600

    fix: error checking

--- frontend/app/app/pharmacist/page.js
+++ frontend/app/app/pharmacist/page.js
@@ -5,9 +5,13 @@ import { useCurrentUser } from '../../hooks/useCurrentUser';
 
 
 const Page = () => {
-  
+
   const userData = useCurrentUser();
-  const pharmacyName = userData.data.user.name
+  const pharmacyName =  null;
+
+  if (userData) {
+    const pharmacyName = userData.data.user.na
+  }
   
   return (
 <div className="px-8 pt-8 pb-0 flex flex-col justify-start md:justify-end items-center min-h-full gap-4">
@@ -18,7 +22,7 @@ const Page = () => {
     </div>
     <div className="px-0 md:px-8 md:pt-8 pb-0 flex-col justify-center items-center w-full">
       <h1 className=" text-xl text-center md:text-4xl md:text-left font-sfProDisplay font-medium mb-8">
-      {pharmacyName}
+      {pharmacyName || "Loading Pharmacy Name"} 
       </h1>
     </div>
     <div className="px-0 md:px-8 pt-0 pb-8 flex md:flex-1 w-full md:h-full h-60">
