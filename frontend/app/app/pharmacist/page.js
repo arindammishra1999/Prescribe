37adc4d675edafcebb99d57e48bbab519c26b268
deost <sajid.hafiz1@ucalgary.ca>
2024-04-12 15:48:02 -0600

    fix: change login function to output all the data

--- frontend/app/app/pharmacist/page.js
+++ frontend/app/app/pharmacist/page.js
@@ -1,24 +1,16 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPills } from "@fortawesome/free-solid-svg-icons";
-import { QRCodeSVG } from "qrcode.react";
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { usePharmacyNames } from '../../hooks/useGetPharmacyNames'; 
+import { useCurrentUser } from '../../hooks/useCurrentUser'; 
+
+
 const Page = () => {
-  // Mock data for demonstration
-  const recentTransaction = {
-    date: "2023-03-30",
-    amount: "$100",
-    description: "Payment Received",
-  };
-
-  const userInfo = {
-    fullName: "John Doe",
-    // More user info can be added here
-  };
-
-
-
+  
+  const userData = useCurrentUser();
+  const pharmacyName = userData.data.user.name
+  
   return (
-    
 <div className="px-8 pt-8 pb-0 flex flex-col justify-start md:justify-end items-center min-h-full gap-4">
     <div className="logo md:hidden justify-self-start">
       <h1 className="text-4xl text-center font-bold font-fredoka text-teal-900">
@@ -27,7 +19,7 @@ const Page = () => {
     </div>
     <div className="px-0 md:px-8 md:pt-8 pb-0 flex-col justify-center items-center w-full">
       <h1 className=" text-xl text-center md:text-4xl md:text-left font-sfProDisplay font-medium mb-8">
-        Welcome Brentwood Pharmacy!
+      {pharmacyName}
       </h1>
     </div>
     <div className="px-0 md:px-8 pt-0 pb-8 flex md:flex-1 w-full md:h-full h-60">
@@ -38,8 +30,6 @@ const Page = () => {
     </div>
 
 </div>
-
-
 
   );
 };
