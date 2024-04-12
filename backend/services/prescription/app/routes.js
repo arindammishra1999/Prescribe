1aa795504f85d7e921e4b3d391138c865db1d579
Rohan Amjad <rohan.amjad@ucalgary.ca>
2024-04-11 21:42:21 -0600

    PRES-26 Implement endpoints to get prescriptions based on pharmacy id and status

--- backend/services/prescription/app/routes.js
+++ backend/services/prescription/app/routes.js
@@ -3,6 +3,10 @@ const {
     getPrescriptionsByStatusAndPatientIdController,
 } = require('./controllers/patientPrescriptionController');
 const {
+    getAllPrescriptionsByPharmacyIdController,
+    getPrescriptionsByStatusAndPharmacyIdController,
+} = require('./controllers/pharmacyPrescriptionController');
+const {
     createPrescriptionController,
     updatePrescriptionController,
 } = require('./controllers/prescriptionController.js');
@@ -11,6 +15,8 @@ const router = require('express').Router();
 
 router.get('/patient/:id/prescriptions', authorizeMiddleware(['patient', 'prescriber', 'pharmacy']), getAllPrescriptionsByPatientIdController);
 router.get('/patient/:id/prescription/:status', authorizeMiddleware(['patient', 'prescriber', 'pharmacy']), getPrescriptionsByStatusAndPatientIdController);
+router.get('/pharmacy/:id/prescriptions', authorizeMiddleware(['pharmacy']), getAllPrescriptionsByPharmacyIdController);
+router.get('/pharmacy/:id/prescription/:status', authorizeMiddleware(['pharmacy']), getPrescriptionsByStatusAndPharmacyIdController);
 router.post('/prescriptions', authorizeMiddleware(['prescriber']), createPrescriptionController);
 router.put('/prescription/:id', authorizeMiddleware(['prescriber', 'pharmacy']), updatePrescriptionController);
 
