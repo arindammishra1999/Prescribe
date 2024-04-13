d0ee577263add32bb3ffea9ff23b1de15eff1114
Saud Agha <saud.agha1@ucalgary.ca>
2024-04-12 19:47:55 -0600

    PRES-22
    feat (frontend): setting up authentication for api calls

--- frontend/app/services/axiosInstances.js
+++ frontend/app/services/axiosInstances.js
@@ -1,8 +1,19 @@
 import axios from "axios";
-import API_BASE_URL from "../utils/apiConfig";
+import { API_PRESCRIPTIONS, API_BASE_URL } from "../utils/apiConfig";
+import { useCurrentUser } from "../hooks/useCurrentUser";
 
 export const instance = axios.create({
   baseURL: API_BASE_URL,
+  timeout: 30000,
+  timeoutErrorMessage: "Request timed out",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+export const getInstance = axios.create({
+  baseURL: API_PRESCRIPTIONS,
   timeout: 30000,
   timeoutErrorMessage: "Request timed out",
   headers: {
